3df0485886e35e04dd10fd0b54e9d21f2902dd51
Jesten Herrild <jherrild@expedia.com>
2017-09-24 12:37:06 -0700

    Initial commit - walking skeleton with REST controller

---
+++ backend/src/main/kotlin/power/Machine.kt
@@ -0,0 +1,8 @@
+package power
+
+/**
+ * @author jherrild@expedia.com
+ * Created on 9/24/17
+ */
+class Machine {
+}
---
+++ backend/src/main/kotlin/temp/PID.kt
@@ -0,0 +1,10 @@
+package temp
+
+/**
+ * @author jherrild@expedia.com
+ * Created on 9/24/17
+ */
+class PID(setTemp: Int, currentTemp: Int) {
+    var setTemp = setTemp
+    var currentTemp = currentTemp
+}
